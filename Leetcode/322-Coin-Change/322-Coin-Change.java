d99668e67695fe9a3948979972e8329d56046b63
mar1shell <elhamdaouimar1@gmail.com>
2026-03-29 22:12:26 +0100

    added solution folder for problem 322 in leetcode

---
+++ Leetcode/322-Coin-Change/322-Coin-Change.java
@@ -0,0 +1,21 @@
+class Solution {
+    private Queue<Integer> heap;
+    
+    public int coinChange(int[] coins, int amount) {
+        int[] minCoins = new int[amount + 1];
+
+        for (int i = 1; i < minCoins.length; i++) {
+            minCoins[i] = amount + 1;
+        }
+
+        for (int i = 1; i < minCoins.length; i++) {
+            for (int c : coins) {
+                if (i >= c) {
+                    minCoins[i] = Math.min(minCoins[i], minCoins[i - c] + 1);
+                }
+            }
+        }
+
+        return (minCoins[amount] != amount + 1) ? minCoins[amount] : -1;
+    }
+}
